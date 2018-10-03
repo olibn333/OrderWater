3786ec5a33ee398615cfec75b26638a4c0a91338
OliBN <olibn333@gmail.com>
2018-10-03 13:32:46 +0630

    server only

--- start-all.js
+++
@@ -1,7 +0,0 @@
-const args = [ 'start' ];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
-
-const args2 = [ 'start-server.js' ];
-const opts2 = { stdio: 'inherit', cwd: 'server', shell: true };
-require('child_process').spawn('node', args2, opts2);
