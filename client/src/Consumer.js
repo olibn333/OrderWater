374a4ac24d819c39ce52316c308f266b08be5493
OliBN <olibn333@gmail.com>
2018-10-05 18:46:05 +0630

    coins n box

--- client/src/Consumer.js
+++ client/src/Consumer.js
@@ -27,7 +27,6 @@ class ConsApp extends Component {
   }
 
   //Persistence
-
   componentDidMount = () => {
     this.hydrateStateWithLocalStorage();
     window.addEventListener(
@@ -124,7 +123,7 @@ class ConsApp extends Component {
       <div className="App">
         <Header isHome={false} />
         <Userbox
-          user={this.state.userName}
+          userName={this.state.userName}
           ordersCount={this.state.userOrdersCount}
           bottlesCount={this.state.userBottlesCount}/>
 
--- client/src/UserBox.js
+++ client/src/UserBox.js
@@ -1,57 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class UserBox extends Component {
-  constructor(props) {
-    super(props);
+const UserBox = ({ bottlesCount, userName, ordersCount }) => {
+
+  const coinCount = Math.floor(bottlesCount / 3)
+
+  const openPrizes = (coinCount) => {
+    if (window.confirm("You have " + coinCount + " Coins to Spend! Do you want to spend them?")) { alert("Tough. Spending hasnt been developed yet") }
   }
 
-  
-openPrizes = (coins) => {
-  if(window.confirm("You have " + coins + " Coins to Spend! Do you want to spend them?")) {alert("Tough. Spending hasnt been developed yet")}
-}
-
-  render() {
-    const coins = Math.floor(this.props.bottlesCount / 3)
-    return (
-      <div className="points" onChange={this.props.updateUser}>
-        <div className="boxHeader">{this.props.user}</div>
-        <div className="boxBody">
-          <p>Orders Made: {this.props.ordersCount}</p>
-          <p>Bottles Ordered: {this.props.bottlesCount}</p>
-          <p>Gold Coins: {coins}</p>
-        </div>
-        <CoinPics 
-          count={coins}
-          onClick={this.openPrizes} />
-      </div>
-    )
-  }
-}
-
-const CoinPics = ({onClick, count}) => {
-  const clickCoins = () => {
-    onClick(count)
-  }
-  let renderCoins = []
-  for (let i = 0; i < count; i++) {
-    renderCoins.push(<CoinPic key={i}/>)
-  }
   return (
-    <div 
-      className="coinsContainer" 
-      onClick={()=>onClick(count)}>
-        {renderCoins}
+    <div className="points">
+      <UserDataList
+        {...{ bottlesCount, ordersCount, coinCount }}
+        header={userName} />
+      <CoinPics
+        count={coinCount}
+        onClick={openPrizes} />
+      <SpendButton
+        openSpender={()=>openPrizes(coinCount)} />
+    </div>
+  )
+}
+
+const SpendButton = ({ openSpender }) => {
+  return (
+  <button className="spend button" onClick={openSpender}>Spend Coins?</button>
+  )
+}
+
+const UserDataList = ({ header, ordersCount, bottlesCount, coinCount }) => {
+  return (
+    <div className="boxContainer">
+      <div className="boxHeader">{header}</div>
+      <div className="boxBody">
+        <p>Orders Made: {ordersCount}</p>
+        <p>Bottles Ordered: {bottlesCount}</p>
+        <p>Gold Coins: {coinCount}</p>
+      </div>
+    </div>
+  )
+}
+
+const CoinPics = ({ onClick, count }) => {
+
+  function amountToCoins(num, arr) {
+    var coins = [];
+    for (var i = 0; i < arr.length; i++) {
+      while (num >= arr[i]) {
+        coins.push(arr[i]);
+        num = num - arr[i];
+      }
+    }
+    return coins;
+  }
+
+  const coinsArray = amountToCoins(count, [10, 5, 1])
+
+  return (
+    <div
+      className="coinsContainer"
+      onClick={() => onClick(count)}>
+      {coinsArray.map((coinType, i) => <CoinPic key={i} value={coinType} />)}
     </div>)
 }
 
 
-const CoinPic = () => (
+const CoinPic = ({ value }) => (
   <svg className="coin" viewBox="-3 -3 100 125">
     <g transform="translate(-63.3 -49)">
       <ellipse cx="115" cy="108" rx="43.3" ry="58.5" fill="#540" stroke="black" strokeWidth="3" />
       <ellipse cx="107" cy="108" rx="43.3" ry="58.5" fill="#fc0" stroke="black" strokeWidth="5" />
-      <text x="95" y="130" fill="black">1</text>
+      <text text-anchor="middle" x="105" y="130" fill="black">{value}</text>
     </g>
   </svg>
 
